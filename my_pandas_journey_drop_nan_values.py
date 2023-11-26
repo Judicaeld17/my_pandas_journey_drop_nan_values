f24b84ab45165892937b72dbe8502054480c13db
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-26 10:18:17 +0100

    first commit

---
+++ my_pandas_journey_drop_nan_values.py
@@ -0,0 +1,4 @@
+# TASK: Create a function which receive a pandas dataframe and returns a datafrom without NaN values.
+import pandas as pd
+def my_pandas_journey_drop_nan_values(df):
+    return df.dropna()
